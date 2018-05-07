fda0adb134384032d451dff8cae047d49df81ad2
Aakar Sharma <2016ucs0066@iitjammu.ac.in>
2018-05-07 23:21:05 +0530

    added chat, audio chat and video chat, object swapped.

--- Client/app.js
+++ Client/app.js
@@ -5,150 +5,401 @@ var config = {
     projectId: "pocket-tanks-dfb9e",
     storageBucket: "",
     messagingSenderId: "879042693506"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
-var socket = io();   
+var socket = io(); 
+var yourConn; 
+var stream;
+var dataChannel;
+var sendQueue = [];
 
 //authententication
 var authentication = document.getElementById('auth');
 
-    //register
-    var registerForm = document.getElementById('register-form');
-    var registerUsername = document.getElementById('register-username');
-    var registerEmail = document.getElementById('register-email');
-    var registerPassword = document.getElementById('register-password');
-    var register = document.getElementById('register');
-    var SignIn = document.getElementById('SignIn');
+//register
+var registerForm = document.getElementById('register-form');
+var registerUsername = document.getElementById('register-username');
+var registerEmail = document.getElementById('register-email');
+var registerPassword = document.getElementById('register-password');
+var register = document.getElementById('register');
+var SignIn = document.getElementById('SignIn');
 
-    //login
-    var loginForm = document.getElementById('login-form');
-    var loginEmail = document.getElementById('login-email');
-    var loginPassword = document.getElementById('login-password');
-    var login = document.getElementById('login');
-    var create = document.getElementById('create');
+//login
+var loginForm = document.getElementById('login-form');
+var loginEmail = document.getElementById('login-email');
+var loginPassword = document.getElementById('login-password');
+var login = document.getElementById('login');
+var create = document.getElementById('create');
 
 // main-page
 var mainPage = document.getElementById('main-page');
-    //header
-    var header = document.getElementById('header');
-        var userName = document.getElementById('UserName');
-        var logOut = document.getElementById('logOut');
+//header
+var header = document.getElementById('header');
+var userName = document.getElementById('UserName');
+var logOut = document.getElementById('logOut');
+//video
+var remoteVideo = document.getElementById('remoteVideo');
+//game
+var game = document.getElementById("game");
+// var ctx = document.getElementById("ctx").getContext("2d");
+// ctx.font = '30px Arial';
 
-    //game
-    var game = document.getElementById("game");
-    // var ctx = document.getElementById("ctx").getContext("2d");
-    // ctx.font = '30px Arial';
+//chat
+var chat = document.getElementById('chat');
+var player2Name = document.getElementById('otherPlayerName');
+var voiceChat = document.getElementById('voiceChat');
+var videoChat = document.getElementById('videoChat');
+var chatBox = document.getElementById('chatBox');
+var chat1 = document.getElementById('chat1');
+var chat2 = document.getElementById('chat2');
+var chatInput = document.getElementById('chatInput');
+var chatSubmit = document.getElementById('chatSubmit');
 
-    //chat
-    var chat = document.getElementById('chat');
-        var player2Name = document.getElementById('otherPlayerName');
-        var voiceChat = document.getElementById('voiceChat');
-        var videoChat = document.getElementById('videoChat');
-    var chatBox = document.getElementById('chatBox');
-        var chat1 = document.getElementById('chat1');
-        var chat2 = document.getElementById('chat2');
-    var chatInput = document.getElementById('chatInput');
-    var chatSubmit = document.getElementById('chatSubmit');
+//options
+var options = document.getElementById('options');
+var singlePlayer = document.getElementById('single-player');
+var multiPlayer = document.getElementById('multi-player');
 
-    //options
-    var options = document.getElementById('options');
-    var singlePlayer = document.getElementById('single-player');
-    var multiPlayer = document.getElementById('multi-player');
+//selectPlayer
+var selectPlayer = document.getElementById('selectPlayer');
 
+SignIn.addEventListener('click',e => {
+    registerForm.style.display = 'none';
+    loginForm.style.display = 'inline-block';
+});
 
-    SignIn.addEventListener('click',e => {
-        registerForm.style.display = 'none';
-        loginForm.style.display = 'inline-block';
-    });
+create.addEventListener('click',e => {
+    registerForm.style.display = 'inline-block';
+    loginForm.style.display = 'none';
+});    
 
-    create.addEventListener('click',e => {
-        registerForm.style.display = 'inline-block';
-        loginForm.style.display = 'none';
-    });    
-
-    login.addEventListener('click',e => {
-        //Get username and password
-        const email = loginEmail.value;
-        const pass = loginPassword.value;
+login.addEventListener('click',e => {
+    //Get username and password
+    const email = loginEmail.value;
+    const pass = loginPassword.value;
     
-        const auth = firebase.auth();
-        // Sign in
-        firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
-        .then(function() {
-            auth.signInWithEmailAndPassword(email,pass)
-                .catch(e => console.log(e.mesage));
-        });     
-    });
+    const auth = firebase.auth();
+    // Sign in
+    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+    .then(function() {
+        auth.signInWithEmailAndPassword(email,pass)
+        .catch(e => console.log(e.mesage));
+    });     
+});
+
+register.addEventListener('click',e => {
+    //Get username and password
+    const username = registerUsername.value;
+    const email = registerEmail.value;
+    const pass = registerPassword.value;
+    const auth = firebase.auth();
     
-    register.addEventListener('click',e => {
-        //Get username and password
-        const username = registerUsername.value;
-        const email = registerEmail.value;
-        const pass = registerPassword.value;
-        const auth = firebase.auth();
-        
-        // Sign in
-        firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
-        .then(function() {
-                auth.createUserWithEmailAndPassword(email,pass)
-                .then(function(user) {
-                console.log("Hi "+ username);
-                return user.updateProfile({
-                    displayName: username,
-                });
+    // Sign in
+    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+    .then(function() {
+        auth.createUserWithEmailAndPassword(email,pass)
+        .then(function(user) {
+            console.log("Hi "+ username);
+            return user.updateProfile({
+                displayName: username,
             });
         });
     });
+});
 
-    logOut.addEventListener('click',e => {
-        firebase.auth().signOut()
-        .then(function() {
-            // Sign-out successful.
-            authentication.style.display = 'block';
-            mainPage.style.display = 'none';
-            options.style.display = 'block';
-            game.style.display = 'none';
+logOut.addEventListener('click',e => {
+    firebase.auth().signOut()
+    .then(function() {
+        // Sign-out successful.
+        authentication.style.display = 'block';
+        mainPage.style.display = 'none';
+        options.style.display = 'block';
+        game.style.display = 'none';
+        
+        socket.emit('signOutSuccess',player);            
+        player.id = undefined;
+        player.name = undefined;
+    })
+    .catch(function(error) {
+        // An error happened
+        console.log("logout error");
+    });
+});
 
-            socket.emit('signOutSuccess',player);            
-            player.id = undefined;
-            player.name = undefined;
-        })
-        .catch(function(error) {
-            // An error happened
-            console.log("logout error");
+firebase.auth().onAuthStateChanged(firebaseUser => {
+    if(firebaseUser){
+        authentication.style.display = 'none';
+        mainPage.style.display = 'block';
+        player.id = firebaseUser.uid;
+        player.name = firebaseUser.displayName;
+        userName.innerHTML = "Hi " + player.name;
+        socket.emit('signInSuccess',{firebaseUser});
+        ////////////////////////////////////////////////
+        //********************** 
+        //Starting a peer connection 
+        //********************** 
+        
+        //getting local video stream 
+        navigator.webkitGetUserMedia({ video: true, audio: true }, function (myStream) { 
+            stream = myStream; 
+            
+            //displaying local video stream on the page 
+            // localVideo.srcObject = (stream);
+            
+            //using Google public stun server 
+            var configuration = { 
+                "iceServers": [{ "url": "stun:stun2.1.google.com:19302" }]
+            }; 
+            
+            yourConn = new webkitRTCPeerConnection(configuration);
+
+            // yourConn.ondatachannel = function(event) {
+            //     console.log('Data channel is created!');
+            //     receiveChannel = event.channel;
+            //     receiveChannel.onopen = function() {
+            //       console.log('Data channel is open and ready to be used.');
+            //     };
+            //     receiveChannel.onerror = function (error) { 
+            //         console.log("Ooops...error:", error); 
+            //     }; 
+            //     receiveChannel.onmessage = function (event) {
+            //         if(event.data.type === "chat")
+            //             chatBox.innerHTML += "<div id=\"chat1\">" + event.data.value + "</div>";
+            //         else
+            //             console.log(event.data.value);
+            //     }; 
+            //     receiveChannel.onclose = function () { 
+            //         console.log("data channel is closed"); 
+            //     };
+            //   };
+            
+            //creating data channel 
+            // dataChannel = yourConn.createDataChannel("channel1", {reliable:true}); 
+
+            // setup stream listening 
+            yourConn.addStream(stream); 
+            
+            //when a remote user adds stream to the peer connection, we display it 
+            yourConn.onaddstream = function (e) { 
+                remoteVideo.srcObject = (e.stream); 
+            };
+            
+            // Setup ice handling 
+            yourConn.onicecandidate = function (event) { 
+                if (event.candidate) { 
+                    send({ 
+                        type: "candidate", 
+                        candidate: event.candidate 
+                    }); 
+                } 
+            };  
+            
+        }, function (error) { 
+            console.log(error); 
         });
-    });
-
-    firebase.auth().onAuthStateChanged(firebaseUser => {
-        if(firebaseUser){
-            authentication.style.display = 'none';
-            mainPage.style.display = 'block';
-            player.id = firebaseUser.uid;
-            player.name = firebaseUser.displayName;
-            userName.innerHTML = "Hi " + player.name;
-            socket.emit('signInSuccess',{firebaseUser});
-        }
-        else {
-            console.log('not logged in');
-        }
-    });
-
-    singlePlayer.addEventListener('click',e => {
-        options.style.display = 'none';
-        game.style.display = 'block';
-    });
-
-    multiPlayer.addEventListener('click',e => {
-        options.style.display = 'none';
-        game.style.display = 'block';
-    });
-
-    function showPlayers()
-    {
-        var PLAYER_LIST;
-        socket.on('onlinePlayers',function(data){
-            PLAYERS_LIST = data.players;
-            delete PLAYERS_LIST(player.id);
-        });
+        /////////////////////////////////////////////////
+        
     }
+    else {
+        console.log('not logged in');
+    }
+});
+
+singlePlayer.addEventListener('click',e => {
+    options.style.display = 'none';
+    game.style.display = 'block';
+});
+
+multiPlayer.addEventListener('click',e => {
+    options.style.display = 'none';
+    selectPlayer.style.display = 'block';
+    showPlayers();
+});
+
+videoChat.addEventListener('click',e => {
+    if(stream.getVideoTracks()[0].enabled)
+    {
+        stream.getVideoTracks()[0].enabled = false;
+        remoteVideo.style.display = 'none';
+    }
+    else
+    {
+        stream.getVideoTracks()[0].enabled = true;
+        remoteVideo.style.display = 'block';
+    }
+});
+
+voiceChat.addEventListener('click',e => {
+    if(stream.getAudioTracks()[0].enabled)
+    stream.getAudioTracks()[0].enabled = false;
+    else
+    stream.getAudioTracks()[0].enabled = true;
+});
+
+function showPlayers()
+{
+    var PLAYER_LIST;
+    socket.on('onlinePlayers',function(data){
+        PLAYERS_LIST = data.player;
+        delete PLAYERS_LIST[player.id];
+        selectPlayer.innerHTML = "";
+        for(var i in data.player)
+        {
+            selectPlayer.innerHTML += "<div class=\"onlinePlayers\" id=\"" + data.player[i].id +"\">" + data.player[i].name + "<\div>\n"; 
+        }
+        
+        $('div.onlinePlayers').click(function() {
+            var id = $(this).attr('id');
+            socket.emit('askConnection',data.player[id]);
+            socket.on('connectionEstablish',function(temp){
+                if(temp.id === id){
+                    selectPlayer.style.display = 'none';                
+                    game.style.display = "block";                   
+                    otherPlayer = data.player[id];
+                    //////////////////////////////////
+                    // create an offer 
+                    yourConn.createOffer(function (offer) { 
+                        send({ 
+                            type: "offer", 
+                            offer: offer 
+                        }); 
+                        
+                        yourConn.setLocalDescription(offer); 
+                    }, function (error) { 
+                        alert("Error when creating an offer"); 
+                    });
+                }
+            });
+        });
+    });
+}
+
+socket.on('incommingConnection',function(data){
+    if(confirm(data.name + ' wants to play a game with you?'))
+    {
+        selectPlayer.style.display = 'none';                
+        game.style.display = "block";
+        otherPlayer = data;
+        var gameObject = {
+            'player1': data,
+            'player2': player
+        }
+        socket.emit('approvedConnection',gameObject);
+    }
+    else
+    {
+        console.log('rejected offer');
+    }
+});
+
+/////////////////////////////////////////////////////////////////
+//when we got a message from a signaling server 
+socket.on("message", function (msg) { 
+    console.log("Got message", msg);
+    
+    var data = JSON.parse(msg); 
+    
+    switch(data.type) { 
+        case "login": 
+        handleLogin(data.success); 
+        break; 
+        //when somebody wants to call us 
+        case "offer": 
+        handleOffer(data.offer); 
+        break; 
+        case "answer": 
+        handleAnswer(data.answer); 
+        break; 
+        //when a remote peer sends an ice candidate to us 
+        case "candidate": 
+        handleCandidate(data.candidate); 
+        break; 
+        case "leave": 
+        handleLeave(); 
+        break; 
+        default: 
+        break; 
+    }
+});
+
+
+//alias for sending JSON encoded messages 
+function send(message) { 
+    //attach the other peer username to our messages 
+    if (otherPlayer.id) { 
+        message.playerId = otherPlayer.id;
+    } 
+    
+    socket.emit("message1",JSON.stringify(message)); 
+};
+
+//when somebody sends us an offer 
+function handleOffer(offer) { 
+    yourConn.setRemoteDescription(new RTCSessionDescription(offer));
+    
+    //create an answer to an offer 
+    yourConn.createAnswer(function (answer) { 
+        yourConn.setLocalDescription(answer); 
+        
+        send({ 
+            type: "answer", 
+            answer: answer 
+        }); 
+        
+    }, function (error) { 
+        alert("Error when creating an answer"); 
+    }); 
+};
+
+//when we got an answer from a remote user
+function handleAnswer(answer) { 
+    yourConn.setRemoteDescription(new RTCSessionDescription(answer)); 
+};
+
+//when we got an ice candidate from a remote user 
+function handleCandidate(candidate) { 
+    yourConn.addIceCandidate(new RTCIceCandidate(candidate)); 
+    stream.getAudioTracks()[0].enabled = false;
+    stream.getVideoTracks()[0].enabled = false;    
+};
+
+//when user clicks the "send message" button 
+chatSubmit.addEventListener("click", function (event) { 
+    var val = chatInput.value; 
+    chatBox.innerHTML += "<div id=\"chat2\">" + val + "</div>"; 
+     
+    //sending a message to a connected peer 
+    obj = {
+        "type": "chat",
+        "value": val 
+    };
+    if(otherPlayer.id != undefined)
+        send_obj(obj);
+    else
+        console.log("kuch gadbad hai");
+    chatInput.value = "";
+ });
+
+function send_obj(obj){
+    socket.emit('data_transfer',obj);
+}
+
+socket.on('data_receive',function(data){
+    if(data.type === "chat")
+        chatBox.innerHTML += "<div id=\"chat1\">" + data.value + "</div>";
+    else if(data.type === "swap"){
+        otherPlayer = data.value;
+    }
+    else
+        console.log(data.value);
+})
+
+ setInterval(function(){
+    var obj = {
+        "type": "swap",
+        "value": player
+    };
+    if(otherPlayer.id != undefined)
+        send_obj(obj);
+},1000);
--- Server/server.js
+++ Server/server.js
@@ -1,19 +1,30 @@
 var express = require('express');
 var app = express();
-var serv = require('http').Server(app);
 var path = require("path");
+var fs = require('fs');
+var https = require('https');
+
+var port = process.env.PORT || 8000;
 
 var path1 = path.join(__dirname,"\..");
 app.get('/',function(req, res) {
     res.sendFile(path1 + '/Client/index.html');
 });
 app.use('/Client',express.static(path1 + '/Client'));
- 
-serv.listen(process.env.PORT || 8000);
-console.log("Server started.");
- 
+
+const httpsOptions = {
+    key: fs.readFileSync(path1 + '/Server/security/localhost.key'),
+    cert: fs.readFileSync(path1 + '/Server/security/localhost.cert'),
+    requestCert: false,
+    rejectUnauthorized: false
+}
+const server = https.createServer(httpsOptions,app);
+server.listen(port, () => {
+    console.log('server running at ' + port)
+});
+
 var SOCKET_LIST = {};
- 
+
 var PLAYERS_LIST = {};
 
 PLAYERS_LIST.onConnect = function(data){
@@ -22,17 +33,12 @@ PLAYERS_LIST.onConnect = function(data){
     var player ={
         "id" : id,
         "name" : name,
-        "angle" : undefined,
-        "power" : undefined,
-        "tankX" : undefined,
-        "tankY" : undefined,
         "control" : undefined,
         "state" : undefined,
         "round" : undefined,
         "game_type" : undefined,
         "status" : undefined,
-        "bulletX" : undefined,
-        "bulletY" : undefined
+        "otherPlayerId" : undefined
     };
     console.log(player);
     PLAYERS_LIST[id]= player;
@@ -40,30 +46,153 @@ PLAYERS_LIST.onConnect = function(data){
 PLAYERS_LIST.onDisconnect = function(data){
     delete PLAYERS_LIST[data.id];
 }
- 
-var io = require('socket.io')(serv,{});
+
+var io = require('socket.io')(server,{});
 io.sockets.on('connection', function(socket){
-   
+    
     socket.on('signInSuccess',function(data){
+        console.log("signInSuccess");
         PLAYERS_LIST.onConnect(data);
         socket.id = data.firebaseUser.uid;
         SOCKET_LIST[socket.id] = socket;
     });
-
+    
     socket.on('signOutSuccess',function(data){
         delete SOCKET_LIST[data.id];
         console.log("LogOut " + PLAYERS_LIST[data.id].name);        
         PLAYERS_LIST.onDisconnect(data);
     });
-   
+    
     socket.on('disconnect',function(){
         delete SOCKET_LIST[socket.id];
+        var temp_player = PLAYERS_LIST[socket.id];
         if(PLAYERS_LIST[socket.id] != undefined)
-            console.log("Disconnected " + PLAYERS_LIST[socket.id].name);        
+        console.log("Disconnected " + PLAYERS_LIST[socket.id].name);        
         PLAYERS_LIST.onDisconnect(socket);
-    });  
+        ///////////////////////////////////////////
+        if(temp_player != undefined && temp_player.otherPlayerId != undefined) { 
+            console.log("Disconnecting from ", PLAYERS_LIST[temp_player.otherPlayerId].name); 
+            PLAYERS_LIST[temp_player.otherPlayerId].otherPlayerId = undefined; 
+            var soc = SOCKET_LIST[temp_player.otherPlayerId];
+            if(soc != null) { 
+                sendTo(soc, { 
+                    type: "leave" 
+                }); 
+            }
+        }
+    });
+    
+    socket.on('askConnection',function(data){
+        var socket1 = SOCKET_LIST[data.id];
+        socket1.emit('incommingConnection',PLAYERS_LIST[socket.id]);
+    });
+    
+    socket.on('approvedConnection',function(data){
+        var socket1 = SOCKET_LIST[data.player1.id];
+        socket1.emit('connectionEstablish',PLAYERS_LIST[socket.id]);
+    });
+
+    socket.on('data_transfer', function(data){
+        if(PLAYERS_LIST[socket.id] != undefined && PLAYERS_LIST[socket.id].otherPlayerId != undefined){
+            var socket1 = SOCKET_LIST[PLAYERS_LIST[socket.id].otherPlayerId];
+            socket1.emit('data_receive',data);
+        }
+    })
+    
+    //when server gets a message from a connected user 
+    socket.on('message1', function(message) { 
+        
+        var data; 
+        
+        //accepting only JSON messages 
+        try { 
+            data = JSON.parse(message); 
+        } catch (e) { 
+            console.log("Invalid JSON"); 
+            data = {}; 
+        }
+        
+        //switching type of the user message 
+        switch (data.type) {
+            
+            case "offer": 
+            //for ex. UserA wants to call UserB 
+            console.log("Sending offer to: ", PLAYERS_LIST[data.playerId].name);
+            
+            //if UserB exists then send him offer details 
+            var soc = SOCKET_LIST[data.playerId]; 
+            
+            if(soc != null) { 
+                //setting that UserA connected with UserB 
+                PLAYERS_LIST[socket.id].otherPlayerId = data.playerId; 
+                
+                sendTo(soc, { 
+                    type: "offer", 
+                    offer: data.offer, 
+                    id: socket.id 
+                }); 
+            }
+            
+            break;
+            
+            case "answer": 
+            console.log("Sending answer to: ", PLAYERS_LIST[data.playerId].name); 
+            //for ex. UserB answers UserA 
+            var soc = SOCKET_LIST[data.playerId];
+            
+            if(soc != null) { 
+                PLAYERS_LIST[socket.id].otherPlayerId = data.playerId; 
+                sendTo(soc, { 
+                    type: "answer", 
+                    answer: data.answer 
+                }); 
+            } 
+            
+            break; 
+            
+            case "candidate": //////wrong wrong wrong
+            console.log("Sending candidate to:",PLAYERS_LIST[data.playerId].name); 
+            var soc = SOCKET_LIST[data.playerId];
+            
+            if(soc != null) { 
+                sendTo(soc, { 
+                    type: "candidate", 
+                    candidate: data.candidate 
+                }); 
+            } 
+            
+            break;
+            
+            case "leave": 
+            console.log("Disconnecting from", PLAYERS_LIST[data.playerId].name); 
+            var soc = SOCKET_LIST[data.playerId]; 
+            PLAYERS_LIST[socket.id].otherPlayerId = undefined; 
+            
+            //notify the other user so he can disconnect his peer connection 
+            if(soc != null) {
+                sendTo(soc, { 
+                    type: "leave" 
+                }); 
+            }
+            
+            break;
+            
+            default: 
+            sendTo(socket, { 
+                type: "error", 
+                message: "Command not found: " + data.type 
+            }); 
+            
+            break; 
+        }
+        
+    }); 
 });
- 
+
+function sendTo(socket, message) { 
+    socket.emit("message",JSON.stringify(message)); 
+}
+
 setInterval(function(){
     var pack = {
         player:PLAYERS_LIST,
